3fa1e994f4a0d09cb6c16ce5a097e55383158f5f
n1ce4rt <nice.lol.1994@gmail.com>
2022-11-09 21:45:51 +0300

    change footer href

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -11,7 +11,7 @@ export const Footer = ({theme}) => {
             <ul className={style.contact_bar}>
                 <li><Vk style={{fill: ( theme? 'white' : 'black' )}} width={40} height={40}/><a href="https://vk.com/n1ce4rt1994">VK</a></li>
                 <li><Instagram style={{fill: ( theme? 'white' : 'black' )}} width={40} height={40}/><a href="https://www.instagram.com/n1ce4rt/">Instagram</a></li>
-                <li><Telegram style={{fill: ( theme? 'white' : 'black' )}} width={40} height={40}/><a href="https://www.instagram.com/n1ce4rt/">Telegram</a></li>
+                <li><Telegram style={{fill: ( theme? 'white' : 'black' )}} width={40} height={40}/><a href="https://t.me/N1CE4RT">Telegram</a></li>
             </ul>
 
         </section>
